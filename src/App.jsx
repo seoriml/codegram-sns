40aa641267c8f606cdf9fa05b93cf0967d83a6b1
Seorim Lee <esr619@naver.com>
2024-09-24 16:45:50 +0900

    design: 레이아웃컴포넌트 추가, index.css에 최대너비 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import React from "react";
+import Layout from "./components/ui/Layout";
+
 function App() {
   return (
-    <>
+    <Layout>
       <h1>sns</h1>
-    </>
+    </Layout>
   );
 }
 
---
+++ src/components/ui/Layout.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../../index.css";
+
+const Layout = ({ children }) => {
+  return <div className="container">{children}</div>;
+};
+
+export default Layout;
