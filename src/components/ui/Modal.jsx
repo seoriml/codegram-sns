1f51e546abd373c9df8968da84b90834f3714bb0
Seorim Lee <esr619@naver.com>
2024-09-25 13:14:45 +0900

    feat: Redux에서 상태관리할 수 있도록 모달컴포넌트 업데이트

--- src/components/ui/Modal.jsx
+++ src/components/ui/Modal.jsx
@@ -1,28 +1,47 @@
-//Modal.jsx
-
 import React from "react";
 import styles from "./Modal.module.scss";
 
-const Modal = ({
-  isOpen,
-  title,
-  onCancel,
-  onConfirm,
-  cancelText,
-  confirmText,
-}) => {
+import { useDispatch, useSelector } from "react-redux";
+import { closeModal } from "../../redux/modalSlice";
+
+const Modal = () => {
+  const dispatch = useDispatch();
+  const {
+    isOpen,
+    title,
+    confirmButtonText,
+    cancelButtonText,
+    confirmAction,
+    cancelAction,
+  } = useSelector((state) => state.modal);
+
+
   if (!isOpen) return null;
+
+  const handleConfirm = () => {
+    if (confirmAction) {
+      confirmAction();
+    }
+    dispatch(closeModal());
+  };
+
+  const handleCancel = () => {
+    if (cancelAction) {
+      cancelAction();
+    }
+    dispatch(closeModal());
+  };
 
   return (
     <div className={styles["modal-overlay"]}>
       <div className={styles["modal-content"]}>
         <h2 className={styles["modal-title"]}>{title}</h2>
         <div className={styles["modal-buttons"]}>
-          <button className={styles["button-cancel"]} onClick={onCancel}>
-            {cancelText}
+          <button className={styles["button-cancel"]} onClick={handleCancel}>
+            {cancelButtonText}
           </button>
-          <button className={styles["button-confirm"]} onClick={onConfirm}>
-            {confirmText}
+          <button className={styles["button-confirm"]} onClick={handleConfirm}>
+            {confirmButtonText}
           </button>
         </div>
       </div>
