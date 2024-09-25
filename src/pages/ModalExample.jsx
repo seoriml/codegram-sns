f32d2dfef82ebe5bed144116fc52bb5e4ddef673
Seorim Lee <esr619@naver.com>
2024-09-25 13:17:51 +0900

    feat: 리덕스훅을 가져와서 모달을 사용하도록 예시코드 수정

--- src/pages/ModalExample.jsx
+++ src/pages/ModalExample.jsx
@@ -1,35 +1,32 @@
-//ModalExample.jsx
-
 import React from "react";
-import { useModal } from "../hooks/useModal";
 import Modal from "../components/ui/Modal";
+import { useDispatch, useSelector } from "react-redux";
+import { openModal, closeModal } from "../redux/modalSlice";
 
 const App = () => {
-  const {
-    isOpen,
-    title,
-    confirmButtonText,
-    cancelButtonText,
-    confirmAction,
-    cancelAction,
-    openModal,
-    closeModal,
-  } = useModal();
+  const dispatch = useDispatch();
+  const { isOpen, title, confirmButtonText, cancelButtonText } = useSelector(
+    (state) => state.modal
+  );
 
   const handleOpenModal = () => {
-    openModal({
-      modalTitle: "게시글을 삭제할까요?",
-      onConfirm: () => {
-        alert("삭제되었습니다!");
-        closeModal();
-      },
-      onCancel: () => {
-        alert("취소되었습니다!");
-        closeModal();
-      },
-      confirmText: "삭제",
-      cancelText: "취소",
-    });
+    dispatch(
+      openModal({
+        modalTitle: "게시글을 삭제할까요?",
+        confirmText: "삭제",
+        cancelText: "취소",
+      })
+    );
+  };
+
+  const handleConfirm = () => {
+    alert("삭제되었습니다!");
+    dispatch(closeModal());
+  };
+
+  const handleCancel = () => {
+    alert("취소되었습니다!");
+    dispatch(closeModal());
   };
 
   return (
@@ -38,8 +35,8 @@ const App = () => {
       <Modal
         isOpen={isOpen}
         title={title}
-        onCancel={cancelAction}
-        onConfirm={confirmAction}
+        onCancel={handleCancel}
+        onConfirm={handleConfirm}
         cancelText={cancelButtonText}
         confirmText={confirmButtonText}
       />
