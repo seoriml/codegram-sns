eaae4a574ec8a65ec537704a8ab021694c8479d7
yeon-seong <dd300@naver.com>
2024-09-26 13:04:07 +0900

    design: 공통 버튼 컴포넌트 스타일 제작

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
+import styles from './Button.module.scss';
 
-export default function Button() {
+export default function Button({ children, buttonType }) {
+  const buttonTypesList = {
+    loginEnable: 'login-blue-button',
+    loginDisable: 'login-skyblue-button',
+    signUp: 'login-white-button',
+    nonePage: 'page-blue-button',
+    profileEnable: 'profile-blue-button',
+    profileDisable: 'profile-gray-button',
+    followEnable: 'follow-list-blue-button',
+    followDisable: 'follow-list-gray-button',
+    postEnable: 'post-blue-button',
+    postDisable: 'post-skyblue-button'
+  };
+
   return (
-    <button>공통 버튼 컴포넌트</button>
+    <button className={styles[buttonTypesList[buttonType]]}>{children}</button>
   )
 }
+
+// 컴포넌트에서 사용 방법
+// <Button buttonType="">내용</Button>
