e19f7930038f718b1baca3531e212d3b3e86d0df
yhwoooo <hwyang0919@naver.com>
2024-09-27 01:05:40 +0900

    input 경고창 props로 받아오기

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import styles from "./Input.module.scss";
 
-const Input = ({ label, ...props }) => {
+const Input = ({ label, warningMessage, ...props }) => {
   return (
-    <div className={styles["input-container"]}>
-      <label className={styles["input-label"]}>{label}</label>
-      <input className={styles["input-field"]} {...props} />
+    <div className={styles.inputContainer} style={warningMessage ? { "--warning-message": `"${warningMessage}"` } : {}}>
+      <label className={styles.inputLabel}>{label}</label>
+      <input className={styles.inputField} {...props} />
     </div>
   );
 };
