ca7ced930e30b82c6c7afcd19e1c61ecad4e336b
lia006 <123gpfla@naver.com>
2024-09-26 10:16:42 +0900

    feat: BottomNavigation 컴포넌트 import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "./components/ui/Layout";
+import BottomNavigation from "./components/layout/bot-nav/BottomNavigation";
 
 function App() {
   return (
     <Layout>
       <h1>sns</h1>
+      <BottomNavigation />
     </Layout>
   );
 }
