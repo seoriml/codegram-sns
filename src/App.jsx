3d4f360865dd0b9714f76106ea0483cb12732cf8
lia006 <123gpfla@naver.com>
2024-09-26 10:16:42 +0900

    App.js 파일 병합

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Layout from "./components/ui/Layout";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import BottomNavigation from "./components/layout/bot-nav/BottomNavigation";
 
 function App() {
   return (
     <Provider store={store}>
       <Layout>
         <h1>sns</h1>
+        <BottomNavigation />
       </Layout>
     </Provider>
   );
