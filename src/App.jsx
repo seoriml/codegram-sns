44cb0f78a67284a7698ffb01b3e427ecf1a89439
lia006 <123gpfla@naver.com>
2024-09-26 11:25:01 +0900

    fix: 병합 과정에서 생긴 충돌 오류 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Layout from "./components/ui/Layout";
 import BottomNavigation from "./components/layout/bot-nav/BottomNavigation";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
-    <Layout>
-      <h1>sns</h1>
-      <BottomNavigation />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <h1>sns</h1>
+        <BottomNavigation />
+      </Layout>
+    </Provider>
   );
 }
 
