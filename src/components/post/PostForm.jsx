52bbc6fbaa25a5a4828e1a36558021198d555857
Seorim Lee <esr619@naver.com>
2024-09-27 18:50:06 +0900

    feat: 로그인함수 및 게시글 업로드함수 구현

--- src/components/post/PostForm.jsx
+++ src/components/post/PostForm.jsx
@@ -1,12 +1,79 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import BackButton from "../ui/button/BackButton";
 import ImageUploadButton from "../ui/button/ImageUploadButton";
+import useAPI from "../../hooks/useAPI";
 
 const PostForm = ({ onSubmit }) => {
+  const { data, loading, error, post } = useAPI();
+
   // 상태 관리: 게시글 내용, 이미지 배열, 이미지 미리보기 배열
   const [content, setContent] = useState("");
   const [images, setImages] = useState([]);
   const [previews, setPreviews] = useState([]);
+
+  // 로그인 함수
+  const loginUser = async () => {
+    try {
+      const response = await post(
+        `${import.meta.env.VITE_API_URL}/user/login`,
+        {
+          user: {
+            email: "test@test2.com",
+            password: "String21312",
+          },
+        }
+      );
+
+      console.log("응답 데이터:", response);
+
+      if (response && response.payload.user && response.payload.user.token) {
+        const userData = response.payload.user;
+        localStorage.setItem("token", userData.token);
+      } else {
+        console.error("로그인 실패: 토큰이 없습니다.", response.user);
+      }
+    } catch (err) {
+      console.error("로그인 중 오류 발생:", err);
+    }
+  };
+  useEffect(() => {
+    loginUser();
+  }, []);
+
+  // 게시글 업로드 함수
+  const uploadPost = async () => {
+    const token = localStorage.getItem("token");
+
+    const imageString = images
+      .map((image) => URL.createObjectURL(image))
+      .join(",");
+
+    try {
+      // 게시글 업로드 요청
+      const response = await post(
+        `${import.meta.env.VITE_API_URL}/post`,
+        {
+          post: {
+            content: content,
+            image: imageString,
+          },
+        },
+        "application/json",
+        token
+      );
+      const json = await response.json();
+      console.log("게시글 업로드 성공:", json);
+      onSubmit();
+    } catch (err) {
+      console.error("게시글 업로드 중 오류 발생:", err);
+    }
+  };
+
+  // 폼 제출 핸들러
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await uploadPost();
+  };
 
   // 게시글 내용 변경 핸들러
   const handleContentChange = (e) => {
@@ -27,12 +94,6 @@ const PostForm = ({ onSubmit }) => {
     } else {
       alert("최대 3장의 이미지만 업로드할 수 있습니다.");
     }
-  };
-
-  // 폼 제출 핸들러
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onSubmit();
   };
 
   // 이미지 제거 핸들러
