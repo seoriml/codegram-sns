da0bb76c3a7291ee5e9ec3aca8be905de8a7e593
yeon-seong <dd300@naver.com>
2024-09-30 09:56:59 +0900

    design: 공통 버튼 컴포넌트 수정

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,24 +1,75 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-export default function Button({ children, buttonType }) {
-  const buttonTypesList = {
-    loginEnable: 'login-blue-button',
-    loginDisable: 'login-skyblue-button',
-    signUp: 'login-white-button',
-    nonePage: 'page-blue-button',
-    profileEnable: 'profile-blue-button',
-    profileDisable: 'profile-gray-button',
-    followEnable: 'follow-list-blue-button',
-    followDisable: 'follow-list-gray-button',
-    postEnable: 'post-blue-button',
-    postDisable: 'post-skyblue-button'
-  };
+export default function Button({
+  children, onClick,
+  type, padding, border, borderRadius,
+  fontSize, fontWeight, fontColor, backgroundColor
+})
+{
+  const buttonType = {
+    buttonBasic: 'buttonBasic'
+  }
+
+  const buttonPadding = {
+    paddingLogin: 'paddingLogin',
+    paddingFeed: 'paddingFeed',
+    paddingProfile: 'paddingProfile',
+    paddingFollow: 'paddingFollow',
+    paddingPost: 'paddingPost'
+  }
+  
+  const buttonBorder = {
+    borderBlue: 'borderBlue',
+    borderGray: 'borderGray'
+  }
+
+  const buttonBorderRadius = {
+    borderRadiusLarge: 'borderRadiusLarge',
+    borderRadiusRegular: 'borderRadiusRegular',
+    borderRadiusSmall: 'borderRadiusSmall',
+    borderRadiusPost: 'borderRadiusPost'
+  }
+  
+  const buttonFontSize = {
+    fontSizeLarge: 'fontSizeLarge',
+    fontSizeTitle: 'fontSizeTitle',
+    fontSizeRegular: 'fontSizeRegular', 
+    fontSizeDetail: 'fontSizeDetail',
+    fontSizeSmall: 'fontSizeSmall'  
+  }
+  
+  const buttonFontWeight = {
+    fontWeightBold: 'fontWeightBold'
+  }
+  
+  const buttonFontColor = {
+    fontWhite: 'fontWhite',
+    fontGray: 'fontGray'
+  }
+
+  const buttonColor = {
+    colorEnable: 'colorEnable',
+    colorDisable: 'colorDisable',
+    colorCommon: 'colorCommon'
+  }
+
+  const buttonStyle = `${
+    styles[
+      buttonType[type],
+      buttonPadding[padding],
+      buttonBorder[border],
+      buttonBorderRadius[borderRadius],
+      buttonFontSize[fontSize],
+      buttonFontWeight[fontWeight],
+      buttonFontColor[fontColor],
+      buttonColor[backgroundColor]
+    ]
+  }`;
 
   return (
-    <button className={styles[buttonTypesList[buttonType]]}>{children}</button>
+    <button className={buttonStyle} onClick={onClick}>
+      {children}
+    </button>
   )
 }
-
-// 컴포넌트에서 사용 방법
-// <Button buttonType="">내용</Button>
