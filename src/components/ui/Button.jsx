7c9ee7b2b0b72207570e207051013b1edcb2d7c7
yeon-seong <dd300@naver.com>
2024-10-04 13:21:55 +0900

    design: 공통 버튼 기본타입 지정 및 버튼타입별 scss 수정

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,86 +1,25 @@
 import React from 'react';
 import styles from './Button.module.scss';
 
-export default function ButtonComponent({
-  children, onClick, disabled,
-  buttonBasic = 'buttonBasic',
-  padding = 'paddingProfile',
-  border = 'borderBlue',
-  borderRadius = 'borderRadiusRegular',
-  fontSize = 'fontSizeRegular',
-  fontWeight = 'fontWeightBold',
-  fontColor = 'fontWhite',
-  backgroundColor = 'colorEnable'
-})
-{
-  // Button 컴포넌트 props 확인
-  // console.log({ props })
+const ButtonComponent = ({ children, onClick, disabled, buttonType='buttonBasic' }) => {
 
-  const buttonType = {
-    buttonBasic
-  }
-
-  const buttonPadding = {
-    paddingLogin: 'paddingLogin',
-    paddingFeed: 'paddingFeed',
-    paddingProfile: 'paddingProfile',
-    paddingFollow: 'paddingFollow',
-    paddingPost: 'paddingPost'
-  }
-  
-  const buttonBorder = {
-    borderBlue: 'borderBlue',
-    borderGray: 'borderGray'
-  }
-
-  const buttonBorderRadius = {
-    borderRadiusLarge: 'borderRadiusLarge',
-    borderRadiusRegular: 'borderRadiusRegular',
-    borderRadiusSmall: 'borderRadiusSmall',
-    borderRadiusPost: 'borderRadiusPost'
-  }
-
-  const buttonFontSize = {
-    fontSizeLarge: 'fontSizeLarge',
-    fontSizeTitle: 'fontSizeTitle',
-    fontSizeRegular: 'fontSizeRegular', 
-    fontSizeDetail: 'fontSizeDetail',
-    fontSizeSmall: 'fontSizeSmall'  
-  }
-
-  const buttonFontWeight = {
-    fontWeightBold: 'fontWeightBold'
-  }
-
-  const buttonFontColor = {
-    fontWhite: 'fontWhite',
-    fontGray: 'fontGray'
-  }
-
-  const buttonColor = {
-    colorEnable: 'colorEnable',
-    colorDisable: 'colorDisable',
-    colorCommon: 'colorCommon'
-  }
-
+  // 항상 적용되는 버튼 기본타입은 buttonBasic 입니다.
+  // 만약 buttonType이 buttonLogin 이라면
+  // 해당 컴포넌트 버튼 타입은 styles.buttonLogin가 적용됩니다.
   const buttonStyle = `
-    ${styles[buttonType[buttonBasic]]}
-    ${styles[buttonPadding[padding]]}
-    ${styles[buttonBorder[border]]}
-    ${styles[buttonBorderRadius[borderRadius]]}
-    ${styles[buttonFontSize[fontSize]]} 
-    ${styles[buttonFontWeight[fontWeight]]}
-    ${styles[buttonFontColor[fontColor]]}
-    ${styles[buttonColor[backgroundColor]]}
+    ${styles.buttonBasic} ${styles[buttonType] || styles.buttonBasic}
   `;
 
   return (
-    <button
-      className = {buttonStyle}
-      onClick={onClick}
-      disabled={disabled}
-    >
+    <button className={buttonStyle} onClick={onClick} disabled={disabled}>
       {children}
     </button>
-  )
-}
+  );
+};
+
+export default ButtonComponent;
+
+// 사용 예시
+// <ButtonComponent onClick={() => console.log('로그인 버튼 클릭')} buttonType="buttonLoign" disabled="disabled">
+//   로그인
+// </ButtonComponent>
