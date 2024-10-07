14cceacc37288b923598738326cc0f9c8f251459
lia006 <123gpfla@naver.com>
2024-10-07 18:50:34 +0900

    feat: profile-info 경로 추가

--- src/pages/profile/YourProfile.jsx
+++ src/pages/profile/YourProfile.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ProfileInfo from "../../components/profile/profile-info/ProfileInfo";
+import { useParams } from "react-router-dom";
 
 export default function YourProfile() {
-  return <div>YourProfile</div>;
+  const { accountName } = useParams;
+  return <ProfileInfo isMyProfile={false} accountName={accountName} />;
 }
+
+// 확인용 서버 계정
+// accountname = {"ali"}
+// export default function YourProfile() {
+//   return <ProfileInfo isMyProfile={false} accountname={"ali"} />;
+// }
