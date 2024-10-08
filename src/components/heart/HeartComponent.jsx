6b872a24b7c3a51d4839f0731f341b665dc12ebd
yeon-seong <dd300@naver.com>
2024-10-08 18:10:02 +0900

    feat: 게시물 좋아요 취소 기능 추가

--- src/components/heart/HeartComponent.jsx
+++ src/components/heart/HeartComponent.jsx
@@ -6,23 +6,14 @@ import useAPI from '../../hooks/useAPI';
 
 function HeartComponent({ hearts, postId, hearted }) {
   const [heartCount, setHeartCount] = useState(hearts);
-  const { post } = useAPI();
+  const { post, del } = useAPI();
   const token = localStorage.getItem('userToken');
 
   const handleHeart = async () => {
     console.log({ token });
-
-    // if (body) {
-    //   response.body = JSON.stringify(body);
-    // }
-    // console.log(response);
-    // const result = await response.json();
-    // console.log(result);
-
-    // :post_id → 게시글 번호
     try {
       const response = await post (
-        `${import.meta.env.VITE_API_URL}/post/${postId}/heart`,
+        `${import.meta.env.VITE_API_URL}/post/${postId}/heart`, // postId → 게시글 번호
         "",
         "application/json",
         token
@@ -35,32 +26,27 @@ function HeartComponent({ hearts, postId, hearted }) {
     }
   }
 
-
   const handleUnHeart = async () => {
-    const response = await fetch();
-    console.log(response);
-    // try {
-    //   setHeart(false);
-    //   setUnHeart(true);
-    //   setHeartCount(prev => Math.max(prev - 1, 0));
-    //   console.log(heart, unHeart, '좋아요 취소');
-    // }
-    // catch (error) {
-    //   alert("좋아요 취소 중 에러가 발생했습니다.");
-    //   console.error("좋아요 취소 API 요청 중 에러 발생:", error);
-    // }
+    console.log({ token });
+    try {
+      const response = await del (
+        `${import.meta.env.VITE_API_URL}/post/${postId}/unheart`, // postId → 게시글 번호
+        "application/json",
+        token
+      );
+      console.log(response);
+    }
+    catch (error) {
+      alert("좋아요 취소 API 요청 중 에러 발생");
+      console.error("좋아요 취소 API 요청 중 에러 발생:", error);
+    }
   }
 
-  /*
-    <HeartComponent post={post.id} />
-    비어있는 좋아요 버튼을 누르면 채워진 좋아요 버튼으로 바뀌고 좋아요 상태가 된다
-    채워진 좋아요 버튼을 누르면 비어있는 좋아요 버튼으로 바뀌고 좋아요 취소 상태가 된다
-  */
   return (
     <>
       {hearted ? (
         <button onClick={handleUnHeart}>
-          <img src={fillHeart} alt="이미 좋아요를 한 게시글입니다" />
+          <img src={fillHeart} alt="이미 좋아요를 한 게시글입니다." />
           <span>{heartCount}</span>
         </button>
       ) : (
