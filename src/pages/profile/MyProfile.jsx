0cbc3752f5b2263beb09fe2c267b63f8b01750a4
lia006 <123gpfla@naver.com>
2024-10-10 17:53:33 +0900

    feat: profileTabs 컴포넌트 추가 및 확인용 계정 설정

--- src/pages/profile/MyProfile.jsx
+++ src/pages/profile/MyProfile.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import ProfileInfo from "../../components/profile/profile-info/ProfileInfo";
 import ProfileTabs from "../../components/profile/profile-info/ProfileTabs";
 
 export default function MyProfile() {
+  // const { accountName } = useParams();
+  // console.log(useParams());
   return (
     <div>
       <ProfileInfo isMyProfile={true} />
-      <ProfileTabs />
+      <ProfileTabs accountname={"codegram1"} />
     </div>
   );
 }
+
+// "codegram1"
+// "String2"
