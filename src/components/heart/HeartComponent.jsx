a1d52e70fd7f67095e0803d9af17bd4fb68cd8bc
yeon-seong <dd300@naver.com>
2024-10-10 15:16:55 +0900

    comment: 코드 주석 추가 및 코드 구조 수정

--- src/components/heart/HeartComponent.jsx
+++ src/components/heart/HeartComponent.jsx
@@ -4,7 +4,7 @@ import useAPI from '../../hooks/useAPI';
 import emptyHeart from "../../assets/images/icon_heart.svg";
 import fillHeart from "../../assets/images/icon_heart_fill.svg";
 
-
+// hearts: 초기 좋아요 수, hearted: 사용자의 초기 좋아요 상태
 function HeartComponent({ hearts, postId, hearted }) {
   const [heartCount, setHeartCount] = useState(hearts);
   const [localHearted, setLocalHearted] = useState(hearted);
@@ -23,6 +23,7 @@ function HeartComponent({ hearts, postId, hearted }) {
         "application/json",
         token
       );
+      console.log("게시물 좋아요 성공");
     }
     catch (error) {
       // 게시물 좋아요 API 호출 실패 시 로컬 상태 되돌리기
@@ -31,7 +32,7 @@ function HeartComponent({ hearts, postId, hearted }) {
       alert("좋아요 API 요청 중 에러 발생");
       console.error("좋아요 API 요청 중 에러 발생:", error);
     }
-  }
+  };
 
   // 게시물 좋아요 취소
   const handleUnHeart = async () => {
@@ -44,6 +45,7 @@ function HeartComponent({ hearts, postId, hearted }) {
         "application/json",
         token
       );
+      console.log("게시물 좋아요 취소 성공");
     }
     catch (error) {
       // 게시물 좋아요 취소 API 호출 실패 시 로컬 상태 되돌리기
@@ -52,21 +54,24 @@ function HeartComponent({ hearts, postId, hearted }) {
       alert("좋아요 취소 API 요청 중 에러 발생");
       console.error("좋아요 취소 API 요청 중 에러 발생:", error);
     }
-  }
+  };
 
   return (
     <>
-      {localHearted ? (
-        <button onClick={handleUnHeart}>
-          <img src={fillHeart} alt="이미 좋아요를 한 게시글입니다." />
-          <span>{heartCount}</span>
-        </button>
-      ) : (
-        <button onClick={handleHeart}>
-          <img src={emptyHeart} alt="아직 좋아요를 안 한 게시글입니다." />
-          <span>{heartCount}</span>
-        </button>
-      )}
+      {localHearted
+        ? (
+          <button onClick={handleUnHeart}>
+            <img src={fillHeart} alt="이미 좋아요를 한 게시글입니다." />
+            <span>{heartCount}</span>
+          </button>
+        ) 
+        : (
+          <button onClick={handleHeart}>
+            <img src={emptyHeart} alt="아직 좋아요를 안 한 게시글입니다." />
+            <span>{heartCount}</span>
+          </button>
+        )
+      }
     </>
   )
 }
