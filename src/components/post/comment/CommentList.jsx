6ad338eb6332e6ce2a515e081cc8556fccd42b34
Seorim Lee <esr619@naver.com>
2024-10-11 16:59:52 +0900

    feat: 댓글등록 시간계산함수 추가

--- src/components/post/comment/CommentList.jsx
+++ src/components/post/comment/CommentList.jsx
@@ -12,6 +12,23 @@ import OptionsModal from "../../ui/modal/OptionsModal";
 
 const LIMIT = 10; // 한 번에 불러올 댓글 수
 
+// 댓글등록 시간 계산 함수
+const timeAgo = (date) => {
+  const seconds = Math.floor((new Date() - new Date(date)) / 1000);
+  let interval = Math.floor(seconds / 31536000);
+
+  if (interval > 1) return `${interval}년 전`;
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) return `${interval}개월 전`;
+  interval = Math.floor(seconds / 86400);
+  if (interval > 1) return `${interval}일 전`;
+  interval = Math.floor(seconds / 3600);
+  if (interval > 1) return `${interval}시간 전`;
+  interval = Math.floor(seconds / 60);
+  if (interval > 1) return `${interval}분 전`;
+  return `${seconds}초 전`;
+};
+
 export default function CommentList({ postId }) {
   const dispatch = useDispatch();
   const { get, post, del, token } = useAPI();
@@ -146,6 +163,7 @@ export default function CommentList({ postId }) {
               .map((comment) => (
                 <li key={comment.id}>
                   <p>{comment.content}</p>
+                  <p>{timeAgo(comment.createdAt)}</p>
                   <button onClick={() => handleOpenOptionsModal(comment.id)}>
                     삭제
                   </button>
