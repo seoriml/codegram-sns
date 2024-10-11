d7bcfc25a7de3f671424f9e59297dc73b444e0c0
lia006 <123gpfla@naver.com>
2024-10-11 14:12:41 +0900

    feat: accountname 상태 연결

--- src/pages/profile/MyProfile.jsx
+++ src/pages/profile/MyProfile.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState } from "react";
 import ProfileInfo from "../../components/profile/profile-info/ProfileInfo";
 import ProfileTabs from "../../components/profile/profile-info/ProfileTabs";
 
 export default function MyProfile() {
-  // const { accountName } = useParams();
-  // console.log(useParams());
+  const [myAccountName, setMyAccountName] = useState(null);
+
+  const handleProfileLoad = (profile) => {
+    if (profile && profile.accountname) {
+      setMyAccountName(profile.accountname);
+    }
+  };
+
   return (
     <div>
-      <ProfileInfo isMyProfile={true} />
-      <ProfileTabs accountname={"codegram1"} />
+      <ProfileInfo isMyProfile={true} onProfileLoad={handleProfileLoad} />
+      {myAccountName && <ProfileTabs accountname={myAccountName} />}
     </div>
   );
 }
