d4ab805c3f73e58027cc9b054745f5c4ab31eb1c
yeon-seong <dd300@naver.com>
2024-10-11 16:19:17 +0900

    design: 404 페이지 제작

--- src/pages/error/NotFound.jsx
+++ src/pages/error/NotFound.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import styles from "../error/NotFound.module.scss";
+import buttonStyles from "../../components/ui/Button.module.scss";
+import errorIcon from "../../assets/images/icon_404.svg";
 
 const NotFound = () => {
-  return <div>페이지를 찾을 수 없습니다. :(</div>
+  const handleGoBack = () => {
+    window.history.back();
+  }
+  return (
+    <div className={styles.errorContainer}>
+      <img src={errorIcon} className={styles.errorBalloon} alt="404 Not Found" />
+      <p className={styles.errorText}>페이지를 찾을 수 없습니다. :(</p>
+      <Link onClick={handleGoBack} className={buttonStyles.buttonPage}>이전 페이지</Link>
+    </div>
+  );
 }
 
 export default NotFound;
