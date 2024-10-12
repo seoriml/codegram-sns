2e3ba1c7f2fed68108dafb7483618003cac5d89c
yeon-seong <dd300@naver.com>
2024-10-13 04:06:16 +0900

    design: flex를 사용한 화면 전체높이 중앙정렬

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import Spinner from '/loading.gif';
+import styles from './Loading.module.scss';
 
 const Loading = () => {
   return (
-    <img src={Spinner} alt="로딩 중..." />
+    <div className={styles.loadContainer}>
+      <img src={Spinner} className={styles.loadImage} alt="로딩 중..." />
+    </div>
   );
 }
 
