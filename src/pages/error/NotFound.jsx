f70a8cf911bc7844e55277681d72b1b534346d94
yeon-seong <dd300@naver.com>
2024-10-15 17:18:17 +0900

    design: 이전 페이지 버튼 class명 변경

--- src/pages/error/NotFound.jsx
+++ src/pages/error/NotFound.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../error/NotFound.module.scss";
 import buttonStyles from "../../components/ui/Button.module.scss";
 import errorIcon from "../../assets/images/icon_404.svg";
+import ButtonComponent from "../../components/ui/Button";
 
 const NotFound = () => {
   const handleGoBack = () => {
@@ -11,7 +12,9 @@ const NotFound = () => {
     <div className={styles.errorContainer}>
       <img src={errorIcon} className={styles.errorBalloon} alt="404 Not Found" />
       <h1 className={styles.errorText}>페이지를 찾을 수 없습니다. :(</h1>
-      <button onClick={handleGoBack} className={buttonStyles.buttonPage}>이전 페이지</button>
+      <ButtonComponent onClick={handleGoBack} className={buttonStyles.basicType}>
+        이전 페이지
+      </ButtonComponent>
     </div>
   );
 }
