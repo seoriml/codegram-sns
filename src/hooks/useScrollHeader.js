780700c11df0f5b3d4475dd86b1f98dbd20dfef7
lia006 <123gpfla@naver.com>
2024-10-16 21:58:27 +0900

    refactor: 리덕스 상태 관리 제거 -> state 상태 관리 추가

--- src/hooks/useScrollHeader.js
+++ src/hooks/useScrollHeader.js
@@ -1,31 +1,26 @@
 import { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { hideHeader, showHeader } from "../redux/scrollHeaderSlice";
 
 const useScrollHeader = () => {
-  const dispatch = useDispatch();
-  const isVisible = useSelector((state) => state.scrollHeader.isVisible);
-  const [lastScrollY, setLastScrollY] = useState(0); // 이전 스크롤 위치 저장
+  const [isVisible, setIsVisible] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollY = window.scrollY;
 
-      // 스크롤을 내릴 때 헤더 숨기기, 올릴 때 헤더 보이기
+      // 스크롤 내릴 때 헤더 숨김, 올릴 때 헤더 표시
       if (currentScrollY > lastScrollY) {
-        dispatch(hideHeader());
+        setIsVisible(false); // 헤더 숨김
       } else {
-        dispatch(showHeader());
+        setIsVisible(true); // 헤더 표시
       }
 
       setLastScrollY(currentScrollY);
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY, dispatch]);
+  }, [lastScrollY]);
 
   return isVisible;
 };
-
 export default useScrollHeader;
