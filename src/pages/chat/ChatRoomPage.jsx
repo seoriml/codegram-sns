f4eb0dfda09021109a5daf76b00b0cfdf529e590
yeon-seong <dd300@naver.com>
2024-10-17 16:05:39 +0900

    design: 채팅 페이지 헤더의 우측 메뉴 삭제

--- src/pages/chat/ChatRoomPage.jsx
+++ src/pages/chat/ChatRoomPage.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ChatForm from "../../components/chat/ChatForm";
 import styles from "./ChatRoomPage.module.scss";
 import BackButton from "../../components/ui/button/BackButton";
-import VerticalButton from "../../components/ui/button/VerticalButton";
 
 export default function ChatRoomPage() {
   return (
@@ -12,7 +11,6 @@ export default function ChatRoomPage() {
           <BackButton />
           <h1 className={styles.chatTitle}>코드그램 챗봇</h1>
         </div>
-        <VerticalButton />
       </header>
       <section className={styles.chatFormMarginBottom}>
         <ChatForm />
