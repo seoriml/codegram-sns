daafb7c55254a15de25a5113b34f1cef8110d93f
yeon-seong <dd300@naver.com>
2024-10-17 13:14:48 +0900

    design: 채팅방 페이지 퍼블리싱

--- src/pages/chat/ChatRoomPage.jsx
+++ src/pages/chat/ChatRoomPage.jsx
@@ -1,11 +1,22 @@
 import React from "react";
 import ChatForm from "../../components/chat/ChatForm";
 import styles from "./ChatRoomPage.module.scss";
+import BackButton from "../../components/ui/button/BackButton";
+import VerticalButton from "../../components/ui/button/VerticalButton";
 
 export default function ChatRoomPage() {
   return (
-    <div className={styles.container}>
-      <ChatForm />
-    </div>
+    <>
+      <header className={styles.chatHeader}>
+        <div className={styles.leftGroup}>
+          <BackButton />
+          <h1 className={styles.chatTitle}>코드그램 챗봇</h1>
+        </div>
+        <VerticalButton />
+      </header>
+      <section className={styles.chatFormMarginBottom}>
+        <ChatForm />
+      </section>
+    </>
   );
 }
